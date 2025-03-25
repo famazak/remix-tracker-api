b896825685149a5b763ccc79aecd665c121e9dc4
famazak <zakfama58@gmail.com>
2025-03-25 16:23:44 -0400

    initial deps, setup db model

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import DateTime, func
+from sqlmodel import Column, Field, SQLModel
+
+
+class TrackBronzeBase(SQLModel):
+    character_name: str = Field(max_length=32)
+    realm_name: str = Field(max_length=32)
+    bronze_total: int
+
+
+class TrackBronze(TrackBronzeBase, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    modified_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column=Column(DateTime(), onupdate=func.now()),
+    )
+
+
+class TrackBronzeCreate(TrackBronzeBase):
+    pass
